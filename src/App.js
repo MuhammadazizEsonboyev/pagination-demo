6cb46907063d82f99765717a5a96d629eba1864c
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-03-15 19:34:48 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { Pagination, PaginationItem } from "@mui/material";
+import { Stack } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Col, Container, ListGroup, Row, } from "react-bootstrap";
 
 function App() {
+  const [countries, setCountries] = useState()
+  const [page, setPage] = useState(3)
+  const [limit, setLimit] = useState(5)
+  const [len, setLen] = useState()
+
+
+  const getdata = () => {
+    axios.get(`http://localhost:4000/data?_limit=${limit}&_page=${page}`)
+      .then((data) => {
+        setCountries(data?.data)
+      })
+  }
+
+
+  const allData = () => {
+    axios.get(`http://localhost:4000/data`)
+      .then((data) => {
+        setLen(data?.data.length / limit)
+      })
+    }
+    let pagenum = Math.ceil(len)
+
+  useEffect(() => {
+    getdata()
+    allData()
+
+  }, [limit, page])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Row className="justify-content-center mt-3">
+          <Col xs={8}>
+            <ListGroup>
+              {countries?.map((value) => {
+                return (
+                  <>
+                    <ListGroup.Item>{value.name}</ListGroup.Item>
+                  </>
+                )
+              })}
+            </ListGroup>
+            <Stack spacing={2} className="mt-3">
+              <Pagination
+                color="secondary"
+                count={pagenum}
+                page={page}
+                onChange={(_, num) => setPage(num)}
+                renderItem={(item) => (
+                  <PaginationItem
+                    to={`?_limit=${limit}&page=${page}`}
+                    {...item}
+                  />
+                )}
+              />
+            </Stack>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
